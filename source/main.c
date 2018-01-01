a2794db18a998b253a068a0f9b0cb9f8556e719d
yy0113 <abc7514108@gmail.com>
2018-01-01 21:33:31 +0800

    106360233

--- source/main.c
+++ source/main.c
@@ -6,6 +6,7 @@
 #define QUARTER_SPEED 200
 #define QUARTER_SPEED1 100
 void PalmHeart(void);
+void Giant(void);
 const DWORD Freq[FREQ_CNT1 + 1] = { 0, 262, 294, 330, 349, 392, 440, 494,
 									524, 588, 660, 698, 784, 880, 998,
 									1048, 1176, 1320, 1396, 1568, 1760, 1976 };
@@ -29,7 +30,7 @@ int main()
 			
 			break;
 		case 3:
-			
+			Giant();
 			break;
 		case 4:
 			PalmHeart();
@@ -84,5 +85,41 @@ void PalmHeart(void)
 	{
 		if (PalmHeart_Tempo[i] == Freq[0]) Sleep(PalmHeart_Tempo[i] * QUARTER_SPEED);
 		else Beep(Freq[PalmHeart_Freq[i]], PalmHeart_Tempo[i] * QUARTER_SPEED);
+	}
+}
+
+void Giant(void)
+{
+
+
+	const DWORD PalmHeart_Freq[] = {
+		16, 16, 18, 17, 15, 16, 16, 18, 17, 15,
+		16, 18, 17, 16, 16, 15, 13, 15,
+		16, 18, 17, 15, 16, 16, 18, 17, 15,
+		16, 16, 18, 17, 15, 15, 15, 20, 19,
+		16, 16, 18, 17, 15, 16, 16, 18, 17, 15,
+		16, 18, 17, 16, 16, 15, 13, 15,
+		16, 16, 18, 17, 15, 16, 16, 18, 17, 15,
+		16, 18, 17, 16, 15, 16
+	};
+
+	const DWORD PalmHeart_Tempo[] = {
+		2, 2, 2, 2, 4, 2, 2, 2, 2, 4,
+		2, 2, 2, 2, 2, 2, 2, 2,
+		2, 2, 2, 4, 2, 2, 2, 2, 4,
+		2, 2, 2, 2, 4, 4, 4, 2, 2,
+		2, 2, 2, 2, 4, 2, 2, 2, 2, 4,
+		2, 2, 2, 2, 2, 2, 2, 2,
+		2, 2, 2, 2, 4, 2, 2, 2, 2, 4,
+		2, 2, 2, 2, 4, 6
+
+	};
+
+	int i;
+
+	for (i = 0; i != sizeof(PalmHeart_Freq) / sizeof(PalmHeart_Freq[0]); ++i) {
+		if (PalmHeart_Tempo[i] == Freq[0]) Sleep(PalmHeart_Tempo[i] * QUARTER_SPEED);
+		else Beep(Freq[PalmHeart_Freq[i]], PalmHeart_Tempo[i] * QUARTER_SPEED);
+
 	}
 }
